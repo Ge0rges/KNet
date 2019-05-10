da665d9b1b0ff878abb42989fb866eb2605431a9
MalusGreen <kevin97illu@gmail.com>
2019-05-10 12:12:21 -0400

    Switch to Quadratic Cost

--- wakesleep/src/main_test.py
+++ wakesleep/src/main_test.py
@@ -5,11 +5,11 @@ training_data, validation_data, test_data = mnist_loader.load_only_inputs()
 
 total_data = training_data + validation_data + test_data
 
-net = network.WakeSleep([784, 30, 10])
+net = network.WakeSleep([784, 30, 10], cost=network.QuadraticCost)
 
-for i in xrange(30):
+for i in xrange(10):
     print "Stage", i + 1
-    for j in xrange(30 - i):
+    for j in xrange(10 - i):
         print "Wake Phase ", j + 1
         net.wake_phase(total_data, 1, (i + 1) * 100, 10.0 / (i + 1))
         print "Sleep Phase", j + 1
