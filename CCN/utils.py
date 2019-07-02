bfb4bc0298512bdaaa08fbdda21869dda2351fbe
Pratham Desai <prathamdesai13@gmail.com>
2019-07-02 09:30:23 -0400

    all good for integration, fixed correlation function bugs

--- CCN/utils.py
+++ CCN/utils.py
@@ -2,6 +2,18 @@
 
 import numpy as np
 
+def sigmoid(x):
+    """
+    computes sigmoid activation function
+    """
+    return 1.0 / (1.0 + np.exp(-x))
+
+def sigmoid_grad(x):
+    """
+    computes sigmoid activation function gradient
+    """
+    return sigmoid(x) * (1.0 - sigmoid(x))
+
 def relu(x):
     """
     computes relu activation function
@@ -28,4 +40,14 @@ def l2_loss(y, t):
     """
     err = y - t
     loss = np.sum(np.square(err))
-    return (1.0 / 2 * len(y)) * loss
+    n = len(y)
+    return loss / (2.0 * n)
+
+def l2_loss_grad(y, t):
+    """
+    computes gradient of l2 loss with respect to predictions y
+    """
+
+    n = len(y)
+
+    return (y - t) / n
