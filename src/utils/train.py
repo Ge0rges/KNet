2c7c67588f447493fe8bcc9d243049de030229c9
Ge0rges <ge0rges@ge0rges.com>
2020-03-07 17:51:12 -0500

    Revert "freeze hook"
    This reverts commit 3760cb64ec036ce6488f40d649477a15e16f4dcc.

--- src/utils/train.py
+++ src/utils/train.py
@@ -30,7 +30,7 @@ def one_hot(targets, classes):
             targets_onehot[i][classes.index(t)] = 1
     return targets_onehot
 
-def train(batchloader, model, criterion, all_classes, classes, optimizer = None, penalty = None, test = False, use_cuda = False):
+def train(batchloader, model, criterion, all_classes, classes, optimizer = None, penalty = None, test = False, use_cuda = False, freeze=None):
     # switch to train or evaluate mode
     if test:
         model.eval()
@@ -149,6 +149,8 @@ def trainAE(batchloader, model, criterion, all_classes, classes, optimizer=None,
             # compute gradient and do SGD step
             optimizer.zero_grad()
             loss.backward()
+            if freeze:
+                freeze(model)
             optimizer.step()
 
         # measure elapsed time
@@ -246,3 +248,18 @@ class l1l2_penalty(object):
                     row[j] = param2.data[i, j]
                 penalty += row.norm(2)
         return self.l2_coeff * penalty
+
+
+class freeze(object):
+    def __init__(self, model):
+        self.old_model = model
+
+    def __call__(self, new_model):
+        for ((name1, param1), (name2, param2)) in zip(self.old_model.named_parameters(), new_model.named_parameters()):
+            for i in range(param1.data.shape[0]):
+                if 'bias' in name1:
+                    param2.grad[i] = 0
+                    continue
+
+                for j in range(param1.data.shape[1]):
+                    param2.grad[i, j] = 0
