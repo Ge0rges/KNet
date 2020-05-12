3e5f93be285336afed04871b01bd4cf01b8b4410
Kevin Zheng <kevin97illu@gmail.com>
2020-05-12 18:40:12 -0400

    equaions shapes

--- src/utils/data_loading.py
+++ src/utils/data_loading.py
@@ -122,13 +122,13 @@ def bananacar_abstract_loader(size=(280, 190), batch_size=256, num_workers=0, pi
 
 def equations_loader(batch_size=256, num_workers=0, pin_memory=False):
     eqs = [
-        lambda inputs: 1 if np.sum(inputs) > (len(inputs) * 0.5) else 0,
-        lambda inputs: 1 if np.sum(inputs) < (len(inputs) * 0.75) else 0,
-        lambda inputs: 1 if (len(inputs) * 0.75) > np.sum(inputs) > (len(inputs) * 0.5) else 0
+        lambda inputs: 1.0 if np.sum(inputs) > (len(inputs) * 0.5) else 0.0,
+        lambda inputs: 1.0 if np.sum(inputs) < (len(inputs) * 0.75) else 0.0,
+        lambda inputs: 1.0 if (len(inputs) * 0.75) > np.sum(inputs) > (len(inputs) * 0.5) else 0.0
     ]
 
-    inputs = np.random.rand(10, 10000)
-    targets = np.asarray([[eq(i) for eq in eqs] for i in inputs])
+    inputs = np.random.rand(10000, 10).astype('f')
+    targets = np.asarray([[eq(i) for eq in eqs] for i in inputs]).astype('f')
 
     dataset = torch.utils.data.TensorDataset(torch.from_numpy(inputs), torch.from_numpy(targets))
 
